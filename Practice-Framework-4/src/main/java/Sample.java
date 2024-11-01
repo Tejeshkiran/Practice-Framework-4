9d710caaac9469cf36858dbbaaa179835afda092
Tejesh <tejeshkirant@gmail.com>
2024-11-01 13:44:33 +0530

    IST changes

--- Practice-Framework-4/src/main/java/Sample.java
+++ Practice-Framework-4/src/main/java/Sample.java
@@ -2,5 +2,6 @@ public class Sample {
     public static void main(String[] args) {
         System.out.println("Hi");
         System.out.println("PST hi");
+        System.out.println("IST hi");
     }
 }
